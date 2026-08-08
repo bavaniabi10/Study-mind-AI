934fecac20bfbb10afdfff916f8a3ac9087da425
bavaniabi10 <bavaniabi2006@gmail.com>
2026-08-08 18:18:41 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
 export default function App() {
   return (
-    <div style={{ padding: '40px',
-                 textAlign: 'center', frontFamily: 'sans-
-                   serif' }}>
+    <div>
       <h1>Welcome to study Mind AI!
       </h1>
       <p>App is successfully deployed 
